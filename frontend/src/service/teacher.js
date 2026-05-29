04f75aa213562d871f423c54910679c1da76719d
Sunny Bhardwaj <sunnybhardwaj7318@gmail.com>
2026-05-29 10:23:14 +0530

    all done

--- frontend/src/service/teacher.js
+++ frontend/src/service/teacher.js
@@ -4,32 +4,32 @@ import axios from 'axios';
 
 
 export const addTeacher = async (data) => {
-    let response = await axios.post('http://localhost:3000/addteacher', data)
+    let response = await axios.post('https://blogify-adnl.onrender.com/addteacher', data)
     return response;
 }
 
 
 export const getAllTeacher = async () => {
-    let response = await axios.get('http://localhost:3000/allteacher')
+    let response = await axios.get('https://blogify-adnl.onrender.com/allteacher')
     return response;
 }
 
 export const getSingleTeacherDetails = async (id) => {
-    let response = await axios.get(`http://localhost:3000/getteacherbyid/${id}`)
+    let response = await axios.get(`https://blogify-adnl.onrender.com/getteacherbyid/${id}`)
     return response;
 }
 
 export const editTeacher = async (id, data) => {
-    let response = await axios.patch(`http://localhost:3000/editteacher/${id}`, data)
+    let response = await axios.patch(`https://blogify-adnl.onrender.com/editteacher/${id}`, data)
     return response;
 }
 
 export const getTeacherDetail = async (id, data) => {
-    let response = await axios.get(`http://localhost:3000/detailteacher/${id}`, data)
+    let response = await axios.get(`https://blogify-adnl.onrender.com/detailteacher/${id}`, data)
     return response;
 }
 
 export const deleteSingleTeacherDetails = async (id) => {
-    let response = await axios.delete(`http://localhost:3000/deleteteacher/${id}`)
+    let response = await axios.delete(`https://blogify-adnl.onrender.com/deleteteacher/${id}`)
     return response;
 }
