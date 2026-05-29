65c09ea6ae542d7df8703d204ebccd621f362551
Sunny Bhardwaj <sunnybhardwaj7318@gmail.com>
2026-05-29 10:05:41 +0530

    live url lg gya

--- frontend/src/service/blog.js
+++ frontend/src/service/blog.js
@@ -2,31 +2,31 @@ import axios from 'axios';
 
 
 export const addBlog = async (data) => {
-    let response = await axios.post('http://localhost:3000/addblog', data)
+    let response = await axios.post('https://blogify-adnl.onrender.com/addblog', data)
     return response;
 }
 
 
 export const getAllBlog = async () => {
-    let response = await axios.get('http://localhost:3000/allblog')
+    let response = await axios.get('https://blogify-adnl.onrender.com/allblog')
     return response;
 }
 
 export const getSingleBlogDetails = async (id) => {
-    let response = await axios.get(`http://localhost:3000/getblogbyid/${id}`)
+    let response = await axios.get(`https://blogify-adnl.onrender.com/getblogbyid/${id}`)
     return response;
 }
 
 export const editBlog = async (id, data) => {
-    let response = await axios.patch(`http://localhost:3000/editblog/${id}`, data)
+    let response = await axios.patch(`https://blogify-adnl.onrender.com/editblog/${id}`, data)
     return response;
 }
 export const getBlogDetail = async (id, data) => {
-    let response = await axios.get(`http://localhost:3000/detailblog/${id}`, data)
+    let response = await axios.get(`https://blogify-adnl.onrender.com/detailblog/${id}`, data)
     return response;
 }
 
 export const deleteSingleBlogDetails = async (id) => {
-    let response = await axios.delete(`http://localhost:3000/deleteblog/${id}`)
+    let response = await axios.delete(`https://blogify-adnl.onrender.com/deleteblog/${id}`)
     return response;
 }
--- frontend/src/service/student.js
+++ frontend/src/service/student.js
@@ -4,32 +4,32 @@ import axios from 'axios';
 
 
 export const addStudent = async (data) => {
-    let response = await axios.post('http://localhost:3000/addstudent', data)
+    let response = await axios.post('https://blogify-adnl.onrender.com/addstudent', data)
     return response;
 }
 
 
 export const getAllStudent = async () => {
-    let response = await axios.get('http://localhost:3000/allstudent')
+    let response = await axios.get('https://blogify-adnl.onrender.com/allstudent')
     return response;
 }
 
 export const getSingleStudentDetails = async (id) => {
-    let response = await axios.get(`http://localhost:3000/getstudentbyid/${id}`)
+    let response = await axios.get(`https://blogify-adnl.onrender.com/getstudentbyid/${id}`)
     return response;
 }
 
 export const editStudent = async (id, data) => {
-    let response = await axios.patch(`http://localhost:3000/editstudent/${id}`, data)
+    let response = await axios.patch(`https://blogify-adnl.onrender.com/editstudent/${id}`, data)
     return response;
 }
 
 export const getStudentDetail = async (id, data) => {
-    let response = await axios.get(`http://localhost:3000/detailstudent/${id}`, data)
+    let response = await axios.get(`https://blogify-adnl.onrender.com/detailstudent/${id}`, data)
     return response;
 }
 
 export const deleteSingleStudentDetails = async (id) => {
-    let response = await axios.delete(`http://localhost:3000/deletestudent/${id}`)
+    let response = await axios.delete(`https://blogify-adnl.onrender.com/deletestudent/${id}`)
     return response;
 }
